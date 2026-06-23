faec968ec3c658b6010db5e15f83e915c6a1db2e
kammari-ashritha <kammariashritha@gmail.com>
2026-06-23 14:43:40 +0530

    Added backend

--- backend/weather_backend/urls.py
+++ backend/weather_backend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('weather.urls')),
+    path('https://pma-weather-app-kg8k.onrender.com/api/', include('weather.urls')),
 ]
