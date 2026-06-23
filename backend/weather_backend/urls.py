50add928d631c6bd2d1eee88b43307d62c0e3599
kammari-ashritha <kammariashritha@gmail.com>
2026-06-23 17:01:24 +0530

    fix: correct API URLs and CORS settings

--- backend/weather_backend/urls.py
+++ backend/weather_backend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('https://pma-weather-app-kg8k.onrender.com/api/', include('weather.urls')),
+    path('api/', include('weather.urls')),
 ]
