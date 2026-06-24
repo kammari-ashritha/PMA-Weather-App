ccfdb26794222aeff727ad68157bd680c9255125
kammari-ashritha <kammariashritha@gmail.com>
2026-06-24 17:06:11 +0530

    fix: allow x-user-id in CORS headers

--- backend/weather_backend/settings.py
+++ backend/weather_backend/settings.py
@@ -26,7 +26,18 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'weather_backend.urls'
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_HEADERS = ['content-type', 'accept', 'authorization']
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'x-user-id',
+]
 
 DATABASES = {
     'default': {
